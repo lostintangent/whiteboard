ed8ccb32ba0a6de5a11d0d1d9d9ee6ccf001fe02
Arjun Attam <arjunattam@gmail.com>
2019-04-24 13:10:38 -0700

    Test commit

--- src/webView.ts
+++ src/webView.ts
@@ -1,12 +1,15 @@
 import * as path from "path";
 import * as vscode from "vscode";
 
-export function createWebView(context: vscode.ExtensionContext): vscode.WebviewPanel {
+export function createWebView(
+  context: vscode.ExtensionContext
+): vscode.WebviewPanel {
   const staticResourcePath = path.join(context.extensionPath, "static");
   const webViewBaseUri = vscode.Uri.file(staticResourcePath).with({
     scheme: "vscode-resource"
   });
 
+  const test = "Test string";
   const panel = vscode.window.createWebviewPanel(
     "vsls-whiteboard",
     "Live Share Whiteboard",
